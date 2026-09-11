0fdba97f06147c083b2d62056535ecc5e8148142
Sharon Karina <sharina.1401@gmail.com>
2026-09-11 08:32:16 -0500

    Inicializar proyecto

---
+++ src/main.py
@@ -0,0 +1,9 @@
+def main():
+    print("=================================")
+    print("      TUTOR TOGAF INICIADO")
+    print("=================================")
+    print("Entorno configurado correctamente.")
+
+
+if __name__ == "__main__":
+    main()
